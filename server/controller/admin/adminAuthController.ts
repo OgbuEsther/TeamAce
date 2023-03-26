01e3a9a2aa2a2cdbd29e72ea174b47a811bc12c8
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-26 16:39:10 +0100

    saved admin authentication process

--- server/controller/admin/adminAuthController.ts
+++ server/controller/admin/adminAuthController.ts
@@ -56,3 +56,69 @@ export const adminSignup = async (req: Request, res: Response) => {
       });
     }
   };
+
+
+  //admin sign in
+
+  export const adminSignin = async (req: Request, res: Response) => {
+    try {
+      const { companyEmail, password } = req.body;
+  
+      const admin = await adminAuth.findOne({ companyEmail });
+  
+      return res.status(200).json({
+        message: "Success , admin is logged in",
+        data: admin,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        message: "an error occurred while creating admin",
+        data: error.message,
+      });
+    }
+  };
+
+
+//get all admins
+export const getAllAdmin = async (req: Request, res: Response) => {
+    try {
+      const admin = await adminAuth.find();
+  
+      return res.status(200).json({
+        message: "get all admins",
+        data: admin,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        message: "failed to get admin",
+        data: error,
+        err: error.message,
+      });
+    }
+  };  
+
+
+  export const getOneAdmin = async (req: Request, res: Response) => {
+    try {
+      const admin = await adminAuth.findById(req.params.adminId).populate([
+        {
+          path: "wallet",
+          select: "balance credit debit",
+        },
+        {
+          path: "viewUser",
+        },
+      ]);
+  
+      return res.status(200).json({
+        message: "get one admin",
+        data: admin,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        message: "failed to get one admin",
+        data: error,
+        err: error.message,
+      });
+    }
+  };  
