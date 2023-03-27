5495618b7d1f4ae6ec65fd8295057f53636d71b5
Okwoli-Godwin <okwolig60@gmail.com>
2023-03-27 05:46:43 +0100

    updated

--- client/src/Components/Methods.tsx
+++ client/src/Components/Methods.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import img from "./Assets/old.png"
 
 const Methods = () => {
   return (
@@ -8,7 +9,27 @@ const Methods = () => {
               <Top><h2>Add payment methods in a click.</h2></Top>
               <Boxhold>
                   <Card>
-                      
+                      <Img src={img} />
+                      <Down>
+                          <h3>Enjoy Easypay payment method</h3>
+                          <Started>Get started</Started>
+                      </Down>
+                  </Card>
+
+                  <Card>
+                      <Img src={img} />
+                      <Down>
+                          <h3>Enjoy Easypay payment method</h3>
+                          <Started>Get started</Started>
+                      </Down>
+                  </Card>
+
+                  <Card>
+                      <Img src={img} />
+                      <Down>
+                          <h3>Enjoy Easypay payment method</h3>
+                          <Started>Get started</Started>
+                      </Down>
                   </Card>
               </Boxhold>
           </Wrapper>
@@ -17,9 +38,31 @@ const Methods = () => {
 }
 
 export default Methods
+const Started = styled.div`
+    width: 85%;
+    display: flex;
+    justify-content: flex-end;
+    cursor: pointer;
+    margin-top: 50px;
+`
+const Down = styled.div`
+    width: 100%;
+    padding: 20px;
+    flex-direction: column;
+    h3{
+        font-size: 20px;
+        line-height: 1.4;
+        width: 190px;
+    }
+`
+const Img = styled.img`
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+`
 
 const Card = styled.div`
-    width: 300px;
+    width: 370px;
     display: flex;
     flex-direction: column;
     box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
@@ -30,6 +73,8 @@ const Boxhold = styled.div`
     width: 100%;
     display: flex;
     align-items: center;
+    margin-top: 40px;
+    justify-content: space-between;
 `
 const Top = styled.div`
     h2{
